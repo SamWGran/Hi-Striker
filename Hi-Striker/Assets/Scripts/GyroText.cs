f7733ed347d401571330452e21a4206fe17029a5
SamWGran <45196888+SamWGran@users.noreply.github.com>
2021-12-15 10:36:54 +0100

    used eulerAngles

--- Hi-Striker/Assets/Scripts/GyroText.cs
+++ Hi-Striker/Assets/Scripts/GyroText.cs
@@ -15,6 +15,6 @@ public class GyroText : MonoBehaviour
     }
     void Update()
     {
-        assignedText.text = Input.gyro.attitude.ToString();
+        assignedText.text = Input.gyro.attitude.eulerAngles.ToString();
     }
 }
